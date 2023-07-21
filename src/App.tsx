a814a77df27fe04388b9541b1d310c1f3304f75d
Augenb1ick <augenblick14@yandex.ru>
2023-07-21 14:05:19 +0400

    add open-ai api

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import SpeechRecognition, {
   useSpeechRecognition,
 } from 'react-speech-recognition';
+import Chatbot from './components/Chatbot';
 
 const App: React.FC = () => {
+
+  const [finalText, setFinalText] = useState('');
+
+  
+
   // подключили хук
   const {
     transcript,
@@ -12,24 +18,36 @@ const App: React.FC = () => {
     browserSupportsSpeechRecognition,
   } = useSpeechRecognition();
 
+  useEffect(() => {
+    if (!listening) {
+        setFinalText(transcript);
+        console.log(finalText)
+      }
+  },[transcript, listening, finalText])
+
   // если браузер не поддерживает распознование речи, выдаст такой спан
   if (!browserSupportsSpeechRecognition) {
     return <span>Ваш браузер не поддерживает распознавание речи</span>;
   }
   // так можно сохранять результат отработки распознования
-  if (!listening) {
-    console.log(transcript);
-  }
+  
+  
+  
+  
+
 
   // пример использования в риал-тайм
   return (
-    <div>
-      <p>Microphone: {listening ? 'on' : 'off'}</p>
-      <button onClick={SpeechRecognition.startListening}>Start</button>
-      <button onClick={SpeechRecognition.stopListening}>Stop</button>
-      <button onClick={resetTranscript}>Reset</button>
-      <p>{transcript}</p>
-    </div>
+    <>
+      <div>
+        <p>Microphone: {listening ? 'on' : 'off'}</p>
+        <button onClick={SpeechRecognition.startListening}>Start</button>
+        <button onClick={SpeechRecognition.stopListening}>Stop</button>
+        <button onClick={resetTranscript}>Reset</button>
+        <p>{transcript}</p>
+      </div>
+      <Chatbot textFromVoice={finalText} />
+    </>
   );
 };
 
---
+++ src/components/Chatbot.tsx
@@ -0,0 +1,75 @@
+import React, { FC, useState, useEffect } from 'react';
+
+type ChatbotProps = {
+  textFromVoice: string
+}
+
+const Chatbot: FC<ChatbotProps> = ({ textFromVoice }) => {
+  const [prompt, setPrompt] = useState('');
+  const [apiResponse, setApiResponse] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const apiKey = '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';
+  const APIBody = {
+      "model": "gpt-3.5-turbo",
+      "messages": [{"role": "user", "content": prompt ? prompt : textFromVoice }],
+      "temperature": 0,
+      "max_tokens": 1000,
+      "top_p": 1.0,
+      "frequency_penalty": 0.0,
+      "presence_penalty": 0.0
+  }
+
+  useEffect(() => {
+    textFromVoice && postToGpt()
+  },[textFromVoice])
+
+  async function postToGpt() {
+    setLoading(true)
+    await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + apiKey
+      },
+      body: JSON.stringify(APIBody)
+    }).then((data) => {
+      return data.json();
+    }).then((data) => {
+      console.log(data);
+      setApiResponse(data.choices[0].message.content); 
+    }).finally(() => {setLoading(false)})
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    postToGpt()
+    setPrompt('');
+  };
+
+  return (
+    <>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <textarea
+            type='text'
+            value={prompt}
+            placeholder='Спросить ассистента'
+            onChange={(e) => setPrompt(e.target.value)}
+          ></textarea>
+          <button disabled={loading || prompt.length === 0} type='submit'>
+            Отправить
+          </button>
+          <br />{loading ? 'Генерирую ответ.' : ''}
+        </form>
+      </div>
+      {apiResponse && (
+        <div>
+          <p>Ответ: {apiResponse}</p>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Chatbot;
