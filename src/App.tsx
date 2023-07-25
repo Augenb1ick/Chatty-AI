9c96a98af1debda9784af66472e8279a1c3bffeb
Sasha Anshukov <buenosdias2@yandex.ru>
2023-07-25 11:43:07 +0300

    added header and assisstant popup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
 import { FC, useState } from 'react';
 import VoiceToText from './components/VoiceToText';
+import Header from './components/Header';
 import Chatbot from './components/Chatbot';
+import AssistantPopup from './components/AssistantPopup';
+import './components/styles/App.css';
 
 const App: FC = () => {
   const [text, setText] = useState('');
+  const [isOpenAssistantPopup, setIsOpenAssistantPopup] = useState(false);
+
+  function handleChangeAssistant () {
+    setIsOpenAssistantPopup(true)
+  }
+
+  const closePopups = () => {
+    setIsOpenAssistantPopup(false);
+  };
 
   return (
-    <>
+    <div className="App">
+      <Header handleChangeAssistant={handleChangeAssistant}/>
       <VoiceToText transriptedText={setText} />
       <Chatbot textFromVoice={text} />
-    </>
+      <AssistantPopup isOpen={isOpenAssistantPopup} onClose={closePopups}/>
+    </div>
   );
 };
 
---
+++ src/components/lib/locales/components/index.ts
@@ -0,0 +1,12 @@
+
+import translationHeaderEN from './Header/en.json'
+import translationHeaderRU from './Header/ru.json'
+
+export const translationMergeEN = {
+  ...translationHeaderEN,
+}
+export const translationMergeRU = {
+
+  ...translationHeaderRU,
+
+}
