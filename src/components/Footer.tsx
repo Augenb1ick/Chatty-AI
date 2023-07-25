9c34a1b0cd86cb67477017461e8a8c2d3da6db9e
Sasha Anshukov <buenosdias2@yandex.ru>
2023-07-25 20:41:31 +0300

    added 3steps and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,19 @@
 import { FC } from 'react';
+import logo from '../assets/logo.svg';
 
 import './styles/Footer.css';
 
 const Footer: FC = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <img className="footer__logo" src={logo} alt="логотип"/>
+      <ul className="footer__info">
+        <li><p className="footer__text footer__text_contacts">Здесь будет текст</p></li>
+        <li><p className="footer__text footer__text_contacts">Здесь будет текст</p></li>
+      </ul>
+      <p className="footer__text footer__text_copyright">&copy; 2023 ООО PAWsistant — Сервис для владельцев домашних питомцев</p>
+    </div>
+  )
 };
 
 export default Footer;
--- src/components/StepsInfo.tsx
+++ src/components/StepsInfo.tsx
@@ -1,11 +1,36 @@
 import './styles/StepsInfo.css';
+//import arrow from '../assets/arrow-big.svg';
+
+const description = {
+  main: '3 простых шага, чтобы ваш питомец стал счастливие',
+  one: `Расскажите мне о вашем питомце`,
+  two: 'Расскажите о проблеме или задайте интересующий вас вопрос',
+  three: 'Сделайте своего питомца счастливие',
+}
 
 const StepsInfo = () => {
   return (
   <div className="stepsInfo">
-    <img className="stepsInfo__arrow" />
-    <div className="stepsInfo__description"></div>
-    <div className="stepsInfo__img"></div>
+    {/*<img className="stepsInfo__arrow" src={arrow}/>*/}
+    <div className="stepsInfo__MainDescription">{description.main}</div>
+    <div className="stepsInfo__points">
+    {/*<img className="stepsInfo__curve-line" src={curveLine}/>*/}
+      <ul className="stepsInfo__points-steps">
+      
+        <li className="stepsInfo__points-step">
+          <div className="stepsInfo__ellipse">1</div>
+          <p className="stepsInfo__description">{description.one}</p>
+        </li>
+        <li className="stepsInfo__points-step">
+          <div className="stepsInfo__ellipse">2</div>
+          <p className="stepsInfo__description">{description.two}</p>
+        </li>
+        <li className="stepsInfo__points-step">
+          <div className="stepsInfo__ellipse">3</div>
+          <p className="stepsInfo__description">{description.three}</p>
+        </li>
+      </ul>
+    </div>
   </div>
   );
 };
