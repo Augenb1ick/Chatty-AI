ea035286bee67a14ded4fbfdb036e024704fda9f
Augenb1ick <augenblick14@yandex.ru>
2023-07-25 22:23:59 +0400

    fixed build problems

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react';
+import { FC } from 'react';
 import './styles/Main.css';
 import assistantMain from '../images/assistant+ellipse.png';
 import ChatBotSearch from './ChatBotSearch';
