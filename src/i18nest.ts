b4b62420625cb5829d1a9dcb279c581c220219b7
Augenb1ick <augenblick14@yandex.ru>
2023-07-28 00:31:39 +0400

    turned off i18 debugger

--- src/i18nest.ts
+++ src/i18nest.ts
@@ -1,36 +1,40 @@
-import i18n from 'i18next'
-import LanguageDetector from 'i18next-browser-languagedetector'
-import { initReactI18next } from 'react-i18next'
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from 'react-i18next';
 //import XHR from "i18next-http-backend"
-import { translationMergeEN, translationMergeRU } from './components/lib/locales/components/index'
+import {
+  translationMergeEN,
+  translationMergeRU,
+} from './components/lib/locales/components/index';
 
 i18n
-//.use(XHR) 
-.use(initReactI18next)// передаем экземпляр i18n в react-i18next, который сделает его доступным для всех компонентов через context API.
-.use(LanguageDetector)// с помощью плагина определяем язык пользователя в браузере
-.init({
-  resources: { // передаем переводы текстов интерфейса в формате JSON
-    en: {translations: translationMergeEN,},
-    ru: {translations: translationMergeRU,},
-  },
-  lng: 'ru',
-  fallbackLng: 'en', // если переводы на языке пользователя недоступны, то будет использоваться язык, указанный в этом поле
-  debug: true,
+  //.use(XHR)
+  .use(initReactI18next) // передаем экземпляр i18n в react-i18next, который сделает его доступным для всех компонентов через context API.
+  .use(LanguageDetector) // с помощью плагина определяем язык пользователя в браузере
+  .init({
+    resources: {
+      // передаем переводы текстов интерфейса в формате JSON
+      en: { translations: translationMergeEN },
+      ru: { translations: translationMergeRU },
+    },
+    lng: 'ru',
+    fallbackLng: 'en', // если переводы на языке пользователя недоступны, то будет использоваться язык, указанный в этом поле
+    debug: false,
 
-  // have a common namespace used around the full app
-  ns: ['translations'],
-  defaultNS: 'translations',
+    // have a common namespace used around the full app
+    ns: ['translations'],
+    defaultNS: 'translations',
 
-  keySeparator: false, // we use content as keys
+    keySeparator: false, // we use content as keys
 
-  interpolation: {
-    escapeValue: false, // экранирование уже есть в React, поэтому отключаем
-    formatSeparator: ',',
-  },
+    interpolation: {
+      escapeValue: false, // экранирование уже есть в React, поэтому отключаем
+      formatSeparator: ',',
+    },
 
-  // react: {
-  //   wait: true,
-  // },
-})
+    // react: {
+    //   wait: true,
+    // },
+  });
 
-export default i18n
+export default i18n;
