b4feb8dc93db5d8ebb53b38651dc4db126eec111
Sasha Anshukov <buenosdias2@yandex.ru>
2023-07-28 14:47:01 +0300

    added sticky header, lang buttons, vinnie

---
+++ src/App.tsx
@@ -0,0 +1,89 @@
+import { FC, useState } from 'react';
+import Header from './components/Header';
+import Chatbot from './components/Chatbot';
+import Main from './components/Main';
+import StepsInfo from './components/StepsInfo';
+import PopupWithAssistant from './components/PopupWithAssistant';
+import PopupWithLimits from './components/PopupWithLimits';
+import './components/styles/App.css';
+import Footer from './components/Footer';
+
+const App: FC = () => {
+  const [isOpenAssistantPopup, setIsOpenAssistantPopup] = useState(false);
+  const [isOpenPopupWithLimits, setIsOpenPopupWithLimits] = useState(false);
+  const [activeProfile, setActiveProfile] = useState(0);
+
+  const onSelectedProfile = (index: any) => {
+    setActiveProfile(index);
+  };
+
+  function handleChangeAssistant() {
+    setIsOpenAssistantPopup(true);
+  }
+
+  const closePopups = () => {
+    setIsOpenAssistantPopup(false);
+    setIsOpenPopupWithLimits(false);
+  };
+
+  const [isClicked, setIsClicked] = useState(false);
+  const [isMicroClicked, setIsMicroClicked] = useState(false);
+  const [isFaqOpened, setIsFaqOpened] = useState(false);
+
+  const handleRerender = (value: boolean) => {
+    setIsClicked(value);
+    setIsFaqOpened(false);
+  };
+
+  function clickForButton() {
+    setIsOpenPopupWithLimits(true);
+  }
+  const handleMicroClick = (value: boolean) => {
+    setIsMicroClicked(value);
+  };
+
+  const handleFaqOpen = (value: boolean) => {
+    setIsMicroClicked(false);
+    setIsFaqOpened(value);
+  };
+
+  return (
+    <div className='App'>
+      <Header
+        activeProfile={activeProfile}
+        handleChangeAssistant={handleChangeAssistant} 
+        handleLogoClick={() => {setIsClicked(false)}}
+        clickForButton={clickForButton}
+      />
+      {isClicked ? (
+        <Chatbot
+          activeProfile={activeProfile}
+          isMicroOn={isMicroClicked}
+          isFaqOpened={isFaqOpened}
+        />
+      ) : (
+        <Main
+          onMicroClick={handleMicroClick}
+          isClicked={handleRerender}
+          isFaqOpened={handleFaqOpen}
+          activeProfile={activeProfile}
+        />
+      )}
+      {!isClicked ? <StepsInfo /> : null}
+      {!isClicked ? <Footer /> : null}
+      <PopupWithAssistant 
+        activeProfile={activeProfile}
+        onSelectedProfile={onSelectedProfile}
+        isOpen={isOpenAssistantPopup}
+        onClose={closePopups}
+      />
+      <PopupWithLimits 
+        isOpen={isOpenPopupWithLimits} 
+        onClose={closePopups}
+        textButton={"ПОВТОРИТЬ"} 
+      />
+    </div>
+  );
+};
+
+export default App;
--- src/components/PopupWithAssistant.tsx
+++ src/components/PopupWithAssistant.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
 import doggy from '../images/dog.svg';
-import kitty from '../images/cat.svg';
+import kitty from '../images/vinny.jpg';
+//import kitty from '../images/cat.svg';
 import parrot from '../images/bird.svg';
 import './styles/PopupWithAssistant.css';
 import usePopupClose from '../hooks/usePopupClose';
@@ -8,6 +10,7 @@ import PopupWithForm from './PopupWithForm';
 
 type profileData = {
   image: string;
+  name: any;
   active: boolean;
 };
 
@@ -23,35 +26,43 @@ const PopupWithAssistant: FC<PopupWithAssistantProps> = ({
   onClose,
   activeProfile,
   onSelectedProfile,
-}) => {
+  }) => {
+    
+  const { t } = useTranslation();
   const profileData: profileData[] = [
     {
       image: doggy,
+      name: t('__Oliver__'),
       active: true,
     },
     {
       image: kitty,
+      name:  t('__Vincent__'),
       active: false,
     },
     {
       image: parrot,
+      name:  t('__Gustav__'),
       active: false,
     },
   ];
 
   const profileImage = profileData.map((tab: profileData, i: number) => {
     return (
-      <button
-        type='button'
-        aria-label='changeAssistantButton'
-        className={`popup__AssistantButton ${
-          i === activeProfile ? 'popup__AssistantButton_active' : ''
-        }`}
-        key={i}
-        onClick={() => onSelectedProfile(i)}
-      >
-        <img src={tab.image} alt='изображение животного' />
-      </button>
+        <div className="popup__Assistant-container">
+        <button
+          type='button'
+          aria-label='changeAssistantButton'
+          className={`popup__AssistantButton ${
+            i === activeProfile ? 'popup__AssistantButton_active' : ''
+          }`}
+          key={i}
+          onClick={() => onSelectedProfile(i)}
+        >
+          <img className='popup__AssistantButton-image' src={tab.image} alt='изображение животного' />
+        </button>
+        <p className="popup__Assistant-name">{tab.name}</p>
+      </div>
     );
   });
 
