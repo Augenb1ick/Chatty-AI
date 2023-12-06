6697f9e0b5bd79c1daa8acda3d02fa315587e6e0
Nikita <117741649+Augenb1ick@users.noreply.github.com>
2023-12-06 13:28:10 +0400

    update api key

--- src/components/Chatbot.tsx
+++ src/components/Chatbot.tsx
@@ -1,6 +1,6 @@
 import { FC, useState, useEffect, useRef } from 'react';
 import SpeechRecognition, {
-  useSpeechRecognition,
+    useSpeechRecognition,
 } from 'react-speech-recognition';
 
 import { getSearchData } from '../utills/googleSearchApi';
@@ -18,225 +18,245 @@ import './styles/Chatbot.css';
 import './styles/ChatBotSearch.css';
 
 const Chatbot: FC<ChatBotProps> = ({
-  isMicroOn,
-  isFaqOpened,
-  activeProfile,
-  microIsTurnedOff,
-  isSafari,
-  currentLanguage,
-  popupRecognitionOpen,
+    isMicroOn,
+    isFaqOpened,
+    activeProfile,
+    microIsTurnedOff,
+    isSafari,
+    currentLanguage,
+    popupRecognitionOpen,
 }) => {
-  const { t } = useTranslation();
+    const { t } = useTranslation();
 
-  const {
-    transcript,
-    listening,
-    resetTranscript,
-    browserSupportsSpeechRecognition,
-  } = useSpeechRecognition();
+    const {
+        transcript,
+        listening,
+        resetTranscript,
+        browserSupportsSpeechRecognition,
+    } = useSpeechRecognition();
 
-  const [prompt, setPrompt] = useState('');
-  const [loading, setLoading] = useState(false);
-  const [isMicrophoneAvailable, setIsMicrophoneAvailable] = useState(true);
-  const lastMessageRoleRef = useRef<string | null>(null);
+    const [prompt, setPrompt] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [isMicrophoneAvailable, setIsMicrophoneAvailable] = useState(true);
+    const lastMessageRoleRef = useRef<string | null>(null);
 
-  const assistantName = useAssistantName(activeProfile);
+    const assistantName = useAssistantName(activeProfile);
 
-  const [chatHistory, setChatHistory] = useState<Message[]>([
-    {
-      role: 'system',
-      content: 'Тебя зовут' + assistantName + initOpenAiPromt,
-    },
-  ]);
+    const [chatHistory, setChatHistory] = useState<Message[]>([
+        {
+            role: 'system',
+            content: 'Тебя зовут' + assistantName + initOpenAiPromt,
+        },
+    ]);
 
-  const checkMicrophonePermission = async () => {
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-      setIsMicrophoneAvailable(true);
-      stream.getTracks().forEach((track) => track.stop());
-    } catch (error) {
-      setIsMicrophoneAvailable(false);
-    }
-  };
-
-  useEffect(() => {
-    checkMicrophonePermission();
-  }, []);
-
-  useEffect(() => {
-    handleScroll();
-  });
-
-  const startListening = () => {
-    if (!browserSupportsSpeechRecognition) {
-      popupRecognitionOpen(true);
-    } else {
-      if (isSafari) {
-        SpeechRecognition.startListening({
-          continuous: true,
-          language: `${currentLanguage === 'ru' ? 'ru-RU' : 'en-US'}`,
-        });
-      }
-      SpeechRecognition.startListening({
-        language: `${currentLanguage === 'ru' ? 'ru-RU' : 'en-US'}`,
-      });
-    }
-  };
-
-  useEffect(() => {
-    if (isMicroOn) {
-      startListening();
-    }
-  }, [isMicroOn]);
-
-  useEffect(() => {
-    if (transcript && listening === false) {
-      handleVoiceRecognition();
-    }
-  }, [transcript, listening]);
-
-  useEffect(() => {
-    if (chatHistory.length > 0) {
-      const lastMessage = chatHistory[chatHistory.length - 1];
-      lastMessageRoleRef.current = lastMessage.role;
-    }
-  }, [chatHistory]);
-
-  useEffect(() => {
-    if (chatHistory.length > 1 && lastMessageRoleRef.current !== 'assistant') {
-      SpeechRecognition.stopListening();
-      postToGpt();
-    }
-  }, [chatHistory, lastMessageRoleRef]);
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPrompt(e.target.value);
-  };
-
-  const handleVoiceRecognition = async () => {
-    setPrompt(transcript);
-    setLoading(true);
-
-    try {
-      const enrichedData = await getSearchData(transcript);
-      setChatHistory((prevMessages) => [
-        ...prevMessages,
-        { role: 'user', content: transcript },
-        { role: 'system', content: enrichedData },
-      ]);
-      setLoading(false);
-    } catch (error) {
-      console.error('Ошибка при получении данных их поисковой выдачи', error);
-      setLoading(false);
-    }
-  };
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    if (prompt.trim() !== '') {
-      const enrichedData = await getSearchData(prompt);
-
-      setChatHistory((prevMessages) => [
-        ...prevMessages,
-        { role: 'user', content: prompt },
-        { role: 'system', content: enrichedData },
-      ]);
-
-      setPrompt('');
-    }
-  };
-
-  async function postToGpt() {
-    setPrompt('');
-    resetTranscript();
-    setLoading(true);
-    const APIBody = {
-      model: 'gpt-3.5-turbo',
-      messages: chatHistory,
-      temperature: 0,
-      max_tokens: 2000,
-      top_p: 1.0,
-      frequency_penalty: 0.0,
-      presence_penalty: 0.0,
+    const checkMicrophonePermission = async () => {
+        try {
+            const stream = await navigator.mediaDevices.getUserMedia({
+                audio: true,
+            });
+            setIsMicrophoneAvailable(true);
+            stream.getTracks().forEach((track) => track.stop());
+        } catch (error) {
+            setIsMicrophoneAvailable(false);
+        }
     };
 
-    try {
-      const response = await fetch(
-        'https://api.openai.com/v1/chat/completions',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: 'Bearer ' + apiKey,
-          },
-          body: JSON.stringify(APIBody),
-        }
-      );
+    useEffect(() => {
+        checkMicrophonePermission();
+    }, []);
 
-      const data = await response.json();
-      const res = data.choices[0].message;
-      setChatHistory((prevMessages) => [
-        ...prevMessages,
-        { role: 'assistant', content: res.content },
-      ]);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setLoading(false);
-    }
-  }
+    useEffect(() => {
+        handleScroll();
+    });
 
-  return (
-    <div className='chat-container'>
-      <div className='chat'>
-        <FAQ mainFaqOpen={isFaqOpened} />
-        {listening ? (
-          <div onClick={SpeechRecognition.stopListening} className='bigMicro'>
-            {' '}
-            {isSafari && (
-              <button className='stop-audio-btn'>
-                {t('__Stop recording__')}
-              </button>
-            )}
-          </div>
-        ) : null}
-        <ChatHistory activeProfile={activeProfile} chatHistory={chatHistory} />
-        <form
-          className={`inputArea inputArea-Inchat ${loading ? 'gradient' : ''} `}
-          onSubmit={handleSubmit}
-        >
-          <input
-            autoFocus
-            className='chatInput chatInput-inChat'
-            value={
-              loading ? t('__Generating__') : transcript ? transcript : prompt
+    const startListening = () => {
+        if (!browserSupportsSpeechRecognition) {
+            popupRecognitionOpen(true);
+        } else {
+            if (isSafari) {
+                SpeechRecognition.startListening({
+                    continuous: true,
+                    language: `${currentLanguage === 'ru' ? 'ru-RU' : 'en-US'}`,
+                });
             }
-            placeholder={t('__Спросить ассистента...__')}
-            onChange={handleInputChange}
-            disabled={loading}
-          ></input>
-          <button
-            onClick={() => {
-              if (isMicrophoneAvailable) {
-                startListening();
-              } else {
-                microIsTurnedOff(true);
-              }
-            }}
-            className='microBtn'
-            type='button'
-            disabled={loading}
-          ></button>
-        </form>
-        <button
-          onClick={handleSubmit}
-          className='submitBtn'
-          disabled={loading}
-          type='submit'
-        ></button>
-      </div>
-    </div>
-  );
+            SpeechRecognition.startListening({
+                language: `${currentLanguage === 'ru' ? 'ru-RU' : 'en-US'}`,
+            });
+        }
+    };
+
+    useEffect(() => {
+        if (isMicroOn) {
+            startListening();
+        }
+    }, [isMicroOn]);
+
+    useEffect(() => {
+        if (transcript && listening === false) {
+            handleVoiceRecognition();
+        }
+    }, [transcript, listening]);
+
+    useEffect(() => {
+        if (chatHistory.length > 0) {
+            const lastMessage = chatHistory[chatHistory.length - 1];
+            lastMessageRoleRef.current = lastMessage.role;
+        }
+    }, [chatHistory]);
+
+    useEffect(() => {
+        if (
+            chatHistory.length > 1 &&
+            lastMessageRoleRef.current !== 'assistant'
+        ) {
+            SpeechRecognition.stopListening();
+            postToGpt();
+        }
+    }, [chatHistory, lastMessageRoleRef]);
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPrompt(e.target.value);
+    };
+
+    const handleVoiceRecognition = async () => {
+        setPrompt(transcript);
+        setLoading(true);
+
+        try {
+            const enrichedData = await getSearchData(transcript);
+            setChatHistory((prevMessages) => [
+                ...prevMessages,
+                { role: 'user', content: transcript },
+                { role: 'system', content: enrichedData },
+            ]);
+            setLoading(false);
+        } catch (error) {
+            console.error(
+                'Ошибка при получении данных их поисковой выдачи',
+                error
+            );
+            setLoading(false);
+        }
+    };
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        if (prompt.trim() !== '') {
+            const enrichedData = await getSearchData(prompt);
+
+            setChatHistory((prevMessages) => [
+                ...prevMessages,
+                { role: 'user', content: prompt },
+                { role: 'system', content: enrichedData },
+            ]);
+
+            setPrompt('');
+        }
+    };
+
+    async function postToGpt() {
+        setPrompt('');
+        resetTranscript();
+        setLoading(true);
+        const APIBody = {
+            model: 'gpt-3.5-turbo',
+            messages: chatHistory,
+            temperature: 0,
+            max_tokens: 2000,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+        };
+
+        try {
+            const response = await fetch(
+                'https://api.openai.com/v1/chat/completions',
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: 'Bearer ' + apiKey,
+                    },
+                    body: JSON.stringify(APIBody),
+                }
+            );
+
+            const data = await response.json();
+            const res = data.choices[0].message;
+            setChatHistory((prevMessages) => [
+                ...prevMessages,
+                { role: 'assistant', content: res.content },
+            ]);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    return (
+        <div className='chat-container'>
+            <div className='chat'>
+                <FAQ mainFaqOpen={isFaqOpened} />
+                {listening ? (
+                    <div
+                        onClick={SpeechRecognition.stopListening}
+                        className='bigMicro'
+                    >
+                        {' '}
+                        {isSafari && (
+                            <button className='stop-audio-btn'>
+                                {t('__Stop recording__')}
+                            </button>
+                        )}
+                    </div>
+                ) : null}
+                <ChatHistory
+                    activeProfile={activeProfile}
+                    chatHistory={chatHistory}
+                />
+                <form
+                    className={`inputArea inputArea-Inchat ${
+                        loading ? 'gradient' : ''
+                    } `}
+                    onSubmit={handleSubmit}
+                >
+                    <input
+                        autoFocus
+                        className='chatInput chatInput-inChat'
+                        value={
+                            loading
+                                ? t('__Generating__')
+                                : transcript
+                                ? transcript
+                                : prompt
+                        }
+                        placeholder={t('__Спросить ассистента...__')}
+                        onChange={handleInputChange}
+                        disabled={loading}
+                    ></input>
+                    <button
+                        onClick={() => {
+                            if (isMicrophoneAvailable) {
+                                startListening();
+                            } else {
+                                microIsTurnedOff(true);
+                            }
+                        }}
+                        className='microBtn'
+                        type='button'
+                        disabled={loading}
+                    ></button>
+                </form>
+                <button
+                    onClick={handleSubmit}
+                    className='submitBtn'
+                    disabled={loading}
+                    type='submit'
+                ></button>
+            </div>
+        </div>
+    );
 };
 
 export default Chatbot;
